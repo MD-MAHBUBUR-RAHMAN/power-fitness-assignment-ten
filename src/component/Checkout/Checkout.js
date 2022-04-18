82c722470dc1535e41ddff0f08231764c66c9018
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-04-18 16:44:02 +0600

    read me file update

--- src/component/Checkout/Checkout.js
+++ src/component/Checkout/Checkout.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Checkout = () => {
   return (
-    <div>
-      <h3>check out now</h3>
+    <div className="container d-flex">
+      <div
+        style={{ height: "1px" }}
+        className="mt-5 bg-danger w-50
+        "
+      ></div>
+      <h3 className="text-center text-danger mx-4 mt-3">Check Out Now</h3>
+      <div
+        style={{ height: "1px" }}
+        className="mt-5 bg-danger w-50
+        "
+      ></div>
     </div>
   );
 };
--- src/component/Notfound/Notfound.js
+++ src/component/Notfound/Notfound.js
@@ -1,9 +1,10 @@
 import React from "react";
+import notFound from "../../images/image/found1.jpg";
 
 const Notfound = () => {
   return (
-    <div>
-      <h3>nt found</h3>
+    <div className="container d-flex align-items-center justify-content-center">
+      <img className="img-fluid" src={notFound} alt="" />
     </div>
   );
 };

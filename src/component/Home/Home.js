4bd6159e0b7d7c698fa55bb3d98322820b7c4f2b
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-04-18 12:44:41 +0600

    add why section

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Services from "../Services/Services";
+import Whyneed from "../Whyneed/Whyneed";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <Whyneed />
       <Services />
     </>
   );
---
+++ src/component/Whyneed/Whyneed.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Whyneed = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">
+        WHY <span className="text-danger">POWER FITNESS</span> PERSONAL TRAINING
+        WILL WORK FOR YOU
+      </h2>
+      <p className="text-center mb-5">
+        A tonne of useful advice, guidance and coaching across the 4 keys to
+        success that will ​help you: ​
+      </p>
+      <hr />
+      <div className="text-center">
+        <p> Eat better, without dieting or feeling deprived.</p>
+        <p>Get active, no matter what shape you’re in now.</p>
+        <p> Ditch the food rules, drop the fad diets and conflicting advice.</p>
+        <p> Build fitness into your life, without it taking over.</p>
+        <p>Achieve, and maintain, your goals, even when life gets busy.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Whyneed;
